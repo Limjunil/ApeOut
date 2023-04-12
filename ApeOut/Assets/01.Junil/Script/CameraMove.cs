270202826b32780830278088171d4410046cbb7b
Junil <im5820795@naver.com>
2023-04-12 10:46:09 +0900

    [Junil] Fix Camera Move to mouse postion

--- ApeOut/Assets/01.Junil/Script/CameraMove.cs
+++ ApeOut/Assets/01.Junil/Script/CameraMove.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
 
-    public const float SPEED_CAMERA = 1f;
+    public const float SPEED_CAMERA = 2f;
 
     public float cameraHeight = default;
     public float cameraWidth = default;
@@ -15,8 +15,6 @@ public class CameraMove : MonoBehaviour
     // 예외 거리 값
     public float exceptionRangeVal = default;
 
-    // 플레이어와 카메라 사이의 거리 값
-    public float distFromCamera = default;
 
     public Vector3 offset = default;
 
@@ -28,7 +26,6 @@ public class CameraMove : MonoBehaviour
 
         exceptionRangeVal = 0.4f;
 
-        distFromCamera = 8f;
 
         offset = new Vector3(0f, 8f, 0f);
 
@@ -39,9 +36,42 @@ public class CameraMove : MonoBehaviour
 
     private void LateUpdate()
     {
-        gameObject.transform.position = targetPlayer.transform.position + offset;
+        //gameObject.transform.position = targetPlayer.transform.position + offset;
+
+        var mousPosTest_ = Input.mousePosition;
+        
+        mousPosTest_.z = Camera.main.nearClipPlane + 7;
+
+        Vector3 mousePos_ = Camera.main.ScreenToWorldPoint(mousPosTest_);
+
+        Vector3 targetPos_ = targetPlayer.transform.position;
 
 
+
+        // 마우스 위치에 대한 최대, 최소 값을 구한 값
+        float clampMinX = Mathf.Clamp(mousePos_.x,
+            targetPos_.x - (cameraWidth * 0.5f), targetPos_.x + (cameraWidth * 0.5f));
+
+        float clampMinZ = Mathf.Clamp(mousePos_.z,
+            targetPos_.z - (cameraHeight * 0.5f), targetPos_.z + (cameraHeight * 0.5f));
+
+
+        // 새로운 카메라 위치를 잡아주기 위한 값들
+        Vector3 testCamera_ = new Vector3(clampMinX, 8f, clampMinZ);
+        Vector3 targetCameraPos_ = new Vector3(targetPos_.x, 8f, targetPos_.z);
+
+        // 예외 범위를 지정하여 카메라 위치를 잡는 조건
+        if ((targetPos_.x - (cameraWidth * exceptionRangeVal) <= clampMinX && clampMinX <= targetPos_.x + (cameraWidth * exceptionRangeVal)) &&
+            (targetPos_.z - (cameraHeight * exceptionRangeVal) <= clampMinZ && clampMinZ <= targetPos_.z + (cameraHeight * exceptionRangeVal)))
+        {
+            // 부드러운 움직임을 위해서 Vector3.Lerp 사용
+            gameObject.transform.position = Vector3.Lerp(transform.position, targetCameraPos_, SPEED_CAMERA * Time.deltaTime);
+        }   // if : 예외 범위일 때는 플레이어 위치를 잡는 조건
+        else
+        {
+            gameObject.transform.position = Vector3.Lerp(transform.position, testCamera_, SPEED_CAMERA * Time.deltaTime);
+
+        }   // else : 그 외는 마우스 위치를 잡는 조건
     }
 
 
